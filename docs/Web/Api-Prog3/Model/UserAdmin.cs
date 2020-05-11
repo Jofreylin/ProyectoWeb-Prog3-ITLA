80f6a22c6a6a62ce197171fb3348d4d2090d76ad
gregorix <gregorixpd0809@gmail.com>
2020-05-11 17:04:19 -0400

    Create UserAdmin.cs
    Añadí el modelo UserAdmin

---
+++ docs/Web/Api-Prog3/Model/UserAdmin.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Model
+{
+    [Table("USER_ADMIN")]
+    public class UserAdmin
+    {
+        [Key]
+        [Column("ID")]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(90)]
+        public string Usuario { get; set; }
+        [Required]
+        [StringLength(90)]
+        public string Contraseña { get; set; }
+        [Column("Fecha_Creacion", TypeName = "datetime")]
+        public DateTime FechaCreacion { get; set; }
+        [Required]
+        [StringLength(120)]
+        public string Nombre { get; set; }
+        [StringLength(90)]
+        public string Email { get; set; }
+    }
+}
