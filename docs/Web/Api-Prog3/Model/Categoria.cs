b663b387de4e484d8b85354ef8b7fcbc030782a3
Jofreylin <trabajosjofrey@hotmail.com>
2020-05-11 16:25:08 -0400

    Añadido: Models
    Categoria, Ciudad, Pais, Post, TipoTrabajo

---
+++ docs/Web/Api-Prog3/Model/Categoria.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Model
+{
+    [Table("CATEGORIA")]
+    public class Categoria
+    {
+        public Categoria()
+        {
+            Post = new HashSet<Post>();
+        }
+
+        [Key]
+        [Column("ID")]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(60)]
+        public string Nombre { get; set; }
+
+        [InverseProperty("NombreCategoriaNavigation")]
+        public virtual ICollection<Post> Post { get; set; }
+    }
+}
---
+++ docs/Web/Api-Prog3/Model/TipoTrabajo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Model
+{
+    [Table("TIPO_TRABAJO")]
+    public class TipoTrabajo
+    {
+        public TipoTrabajo()
+        {
+            Post = new HashSet<Post>();
+        }
+
+        [Key]
+        [Column("ID")]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(60)]
+        public string Nombre { get; set; }
+
+        [InverseProperty("NombreTipoTrabajoNavigation")]
+        public virtual ICollection<Post> Post { get; set; }
+    }
+}
