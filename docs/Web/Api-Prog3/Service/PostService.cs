1210d4e43891dc68ee97c1db6eabacfe043746d3
Jofreylin <trabajosjofrey@hotmail.com>
2020-05-21 19:06:35 -0400

    Añadidos: Servicios
    Post, TipoTrabajo, UserAdmin, UserPoster

---
+++ docs/Web/Api-Prog3/Service/PostService.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Persistence;
+using Model;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace Service
+{
+    public interface IPostService
+    {
+        IEnumerable<Post> GetAll();
+        Post Get(int id);
+        bool Add(Post model);
+        bool Update(Post model);
+        bool Delete(int id);
+    }
+    public class PostService : IPostService
+    {
+        private readonly ProjectDbContext _projectDbContext;
+
+        public PostService(ProjectDbContext projectDbContext)
+        {
+            _projectDbContext = projectDbContext;
+        }
+
+        public bool Add(Post model)
+        {
+            try
+            {
+                _projectDbContext.Add(model);
+                _projectDbContext.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public bool Delete(int id)
+        {
+            try
+            {
+                _projectDbContext.Entry(new Post { Id = id }).State = EntityState.Deleted;
+                _projectDbContext.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public Post Get(int id)
+        {
+            var result = new Post();
+
+            try
+            {
+                result = _projectDbContext.Post.Single(x => x.Id == id);
+            }
+            catch (Exception e)
+            {
+
+            }
+
+            return result;
+        }
+
+        public IEnumerable<Post> GetAll()
+        {
+            var result = new List<Post>();
+
+            try
+            {
+                result = _projectDbContext.Post.ToList();
+            }
+            catch (Exception e)
+            {
+
+            }
+            return result;
+        }
+
+        public bool Update(Post model)
+        {
+            try
+            {
+                var originalModel = _projectDbContext.Post.Single(x => x.Id == model.Id);
+
+                originalModel.Id = model.Id;
+                originalModel.Logo = model.Logo;
+                originalModel.NombreCalle = model.NombreCalle;
+                originalModel.NombreCategoria = model.NombreCategoria;
+                originalModel.NombreCiudad = model.NombreCiudad;
+                originalModel.NombrePais = model.NombrePais;
+                originalModel.NombrePosicion = model.NombrePosicion;
+                originalModel.NombreTipoTrabajo = model.NombreTipoTrabajo;
+                originalModel.Poster = model.Poster;
+                originalModel.Descripcion = model.Descripcion;
+                originalModel.DireccionUrl = model.DireccionUrl;
+                originalModel.FechaCreacion = model.FechaCreacion;
+
+                _projectDbContext.Update(originalModel);
+                _projectDbContext.SaveChanges();
+
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ docs/Web/Api-Prog3/Service/TipoTrabajoService.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Model;
+using Persistence;
+
+namespace Service
+{
+    public interface ITipoTrabajoService
+    {
+        IEnumerable<TipoTrabajo> GetAll();
+        TipoTrabajo Get(int id);
+        bool Add(TipoTrabajo model);
+        bool Update(TipoTrabajo model);
+        bool Delete(int id);
+
+    }
+    public class TipoTrabajoService : ITipoTrabajoService
+    {
+        private readonly ProjectDbContext _projectDbContext;
+
+        public TipoTrabajoService(ProjectDbContext projectDbContext)
+        {
+            _projectDbContext = projectDbContext;
+        }
+
+        public bool Add(TipoTrabajo model)
+        {
+            try
+            {
+                _projectDbContext.Add(model);
+                _projectDbContext.SaveChanges();
+
+            }catch(Exception e)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool Delete(int id)
+        {
+            try
+            {
+                _projectDbContext.Entry(new TipoTrabajo { Id = id }).State = EntityState.Deleted;
+                _projectDbContext.SaveChanges();
+            }catch(Exception e)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public TipoTrabajo Get(int id)
+        {
+            var result = new TipoTrabajo();
+            try
+            {
+                result = _projectDbContext.TipoTrabajo.Single(x => x.Id == id);
+            }
+            catch (Exception e)
+            {
+                
+            }
+            return result;
+        }
+
+        public IEnumerable<TipoTrabajo> GetAll()
+        {
+            var result = new List<TipoTrabajo>();
+            try
+            {
+                result = _projectDbContext.TipoTrabajo.ToList();
+            }catch(Exception e)
+            {
+
+            }
+            return result;
+        }
+
+        public bool Update(TipoTrabajo model)
+        {
+            try
+            {
+                var originalModel = _projectDbContext.TipoTrabajo.Single(x => x.Id == model.Id);
+
+                originalModel.Id = model.Id;
+                originalModel.Nombre = model.Nombre;
+
+                _projectDbContext.Update(originalModel);
+                _projectDbContext.SaveChanges();
+
+            } catch (Exception e)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
