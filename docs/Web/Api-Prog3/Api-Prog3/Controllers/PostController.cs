123eff87f253e7da557b5e2a87fa15bead98fb97
gregorix <gregorixpd0809@gmail.com>
2020-05-29 16:21:08 -0400

    Añadí  los contraladores que faltaban
    UserPoster
    Post
    TipoTrabajo

---
+++ docs/Web/Api-Prog3/Api-Prog3/Controllers/PostController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Model;
+using Service;
+
+namespace Api_Prog3.Controllers
+{
+    public class PostController : Controller
+    {
+        public class PosterController : Controller
+        {
+            private readonly IPostService _IPostService;
+
+            public PosterController(IPostService PostService)
+            {
+                _IPostService = PostService;
+            }
+
+            [HttpGet]
+            public ActionResult<IEnumerable<Post>> GetAll()
+            {
+                return Ok(_IPostService.GetAll());
+            }
+
+            [HttpGet("{id}")]
+            public IActionResult Get(int id)
+            {
+                return Ok(_IPostService.Get(id));
+            }
+
+            [HttpPost]
+            public IActionResult Add([FromBody] Post model)
+            {
+                return Ok(_IPostService.Add(model));
+            }
+
+            [HttpPut]
+            public IActionResult Update([FromBody] Post model)
+            {
+                return Ok(_IPostService.Update(model));
+            }
+
+            [HttpDelete("{id}")]
+            public IActionResult Delete(int id)
+            {
+                return Ok(_IPostService.Delete(id));
+            }
+        }
+    }
+}
---
+++ docs/Web/Api-Prog3/Api-Prog3/Controllers/TipoTrabajoController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Model;
+using Service;
+
+namespace Api_Prog3.Controllers
+{
+    public class TipoTrabajoController : Controller
+        {
+            private readonly ITipoTrabajoService _ITipoTrabajoService;
+
+            public TipoTrabajoController(ITipoTrabajoService TipoTrabajoService)
+            {
+                _ITipoTrabajoService = TipoTrabajoService;
+            }
+
+            [HttpGet]
+            public ActionResult<IEnumerable<TipoTrabajo>> GetAll()
+            {
+                return Ok(_ITipoTrabajoService.GetAll());
+            }
+
+            [HttpGet("{id}")]
+            public IActionResult Get(int id)
+            {
+                return Ok(_ITipoTrabajoService.Get(id));
+            }
+
+            [HttpPost]
+            public IActionResult Add([FromBody] TipoTrabajo model)
+            {
+                return Ok(_ITipoTrabajoService.Add(model));
+            }
+
+            [HttpPut]
+            public IActionResult Update([FromBody] TipoTrabajo model)
+            {
+                return Ok(_ITipoTrabajoService.Update(model));
+            }
+
+            [HttpDelete("{id}")]
+            public IActionResult Delete(int id)
+            {
+                return Ok(_ITipoTrabajoService.Delete(id));
+            }
+        }
+    }
+       
+
