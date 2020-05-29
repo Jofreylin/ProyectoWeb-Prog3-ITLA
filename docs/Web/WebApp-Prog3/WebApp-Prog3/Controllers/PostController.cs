741664c1610832091e11c7ec0b48b338ba3433c6
Jofreylin <trabajosjofrey@hotmail.com>
2020-05-29 16:20:31 -0400

    Added PosterClient, PostClient, PostController
    WebApp : Añadidos UserPosterClient (GetAll), PostClient(GetALL), PostController(Index)

---
+++ docs/Web/WebApp-Prog3/WebApp-Prog3/Controllers/PostController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApp_Prog3.Models;
+
+namespace WebApp_Prog3.Controllers
+{
+    public class PostController : Controller
+    {
+        // GET: Post
+        public ActionResult Index()
+        {
+            PostClient postClient = new PostClient();
+            ViewBag.listPosts = postClient.GetAll();
+            return View();
+        }
+    }
+}
