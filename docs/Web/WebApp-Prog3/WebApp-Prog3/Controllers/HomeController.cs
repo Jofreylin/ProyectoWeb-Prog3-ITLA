bffb12b86211c169d4e46dd9a5e91dcccc52bdb0
Jofreylin <trabajosjofrey@hotmail.com>
2020-06-05 17:17:22 -0400

    Lineas vacias

--- docs/Web/WebApp-Prog3/WebApp-Prog3/Controllers/HomeController.cs
+++ docs/Web/WebApp-Prog3/WebApp-Prog3/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApp_Prog3.Models.Clients;
+using WebApp_Prog3.Models;
 
 namespace WebApp_Prog3.Controllers
 {
@@ -10,6 +12,8 @@ namespace WebApp_Prog3.Controllers
     {
         public ActionResult Index()
         {
+            PostClient postClient = new PostClient();
+            ViewBag.listPosts = postClient.GetAll();
             return View();
         }
 
